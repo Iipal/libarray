24c000e5df9bfe93e961b3f9cd883b8b6ad45f9b
_Ipal Desse <Iipal@localhost.localdomain>
2020-01-05 06:00:55 +0200

    pizdec

--- test/main.c
+++ test/main.c
@@ -1,19 +1,16 @@
-#include "libinterface.h"
-# define LIBI_INTERNAL
-# include "libi_internal.h"
-# undef LIBI_INTERNAL
+# include "libinterface.h"
+
 # include <stdio.h>
 
 int main(void)
 {
-    // Interface a = libi_create_interface("int", -42, libi_get_type(int))
+    Interface i;
+    IInitialize(double, 2.0, &i);
+    Interface *iptr = INew(int, 42);
 
-    printf("%d\n", _Generic(char,
-        char: e_type_c, unsigned char: e_type_uc,
-        short: e_type_s, unsigned short: e_type_us,
-        int: e_type_i, unsigned int: e_type_ui,
-        long: e_type_l, unsigned long: e_type_ul,
-        long long: e_type_ll, unsigned long long: e_type_ull,
-        double: e_type_d, long double: e_type_ld,
-        float: e_type_f, default: e_type_ptr));
+    printf("'%s'[%zu]\n", i.tStr, i.size);
+    printf("'%s'[%zu]\n", iptr->tStr, iptr->size);
+
+    IDelete(iptr, NULL);
+    IDelete(&i, NULL);
 }
